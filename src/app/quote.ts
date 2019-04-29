da9939bb10d6313f37f49d6a74933e120909a0fe
Michael Korir <mikor22@gmail.com>
2019-04-29 10:46:18 +0300

    add a toggle button for details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,6 +3,6 @@ export class Quote {
 
     constructor(public id:number, public quote:string, public author:string, public publisher:String, public upvote:number, public downvote: number,public actualDate:Date)
     
-    {this.showDescription
+    {this.showDescription=false
     }
 }
