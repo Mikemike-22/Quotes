b33f089a077ebc11723dc247bb8f6f4a6df546ad
Michael Korir <mikor22@gmail.com>
2019-04-29 09:12:01 +0300

    add properties to quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,3 @@
 export class Quote {
+    constructor(public id:number, public quote:string, public author:string, public publisher:String, public upvote:number, public downvote: number,public actualDate:Date){}
 }
