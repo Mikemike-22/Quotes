4041d83659c5fe907a5cd37b7c02ead64bbb1271
Michael Korir <mikor22@gmail.com>
2019-04-29 09:24:41 +0300

    create an array property of quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes= [
+    new Quote(1,"An apple a day keeps the doctor away","Not A Doctor","Michael",0,0,new Date(2019,4,29)),
+    new Quote(2,"First solve the problem then write the code","John Johnson","Michael",0,0,new Date (2019,4,29))
+  ]
 
   constructor() { }
 
