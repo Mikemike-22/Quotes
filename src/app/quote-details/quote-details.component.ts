eb2c7a906d5b4cac0b7e68bf29bbff819d0258f9
Michael Korir <mikor22@gmail.com>
2019-04-29 12:34:27 +0300

    create quoteDelete function and create an eventEmitter

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 
 import {Quote} from '../quote'
 
@@ -10,6 +10,12 @@ import {Quote} from '../quote'
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote:Quote;
+  @Output() isComplete= new EventEmitter<boolean>();
+
+  quoteCoomplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
   constructor() { }
 
   ngOnInit() {
